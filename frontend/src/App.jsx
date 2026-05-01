150b0b913b1e80cda288ae087e4373425bd554e5
theuash <theuash@gmail.com>
2026-05-02 01:54:08 +0530

    ofne

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,8 @@ import AdminDashboard from './pages/AdminDashboard';
 import AuditorDashboard from './pages/AuditorDashboard';
 import JudgesDashboard from './pages/JudgesDashboard';
 import PublicDashboard from './pages/PublicDashboard';
-<<<<<<< HEAD
-import FraudEdgeCases from './pages/FraudEdgeCases';
-=======
 import FlaggedTransactions from './pages/FlaggedTransactions';
 import NgoFundSpend from './pages/NgoFundSpend';
->>>>>>> 1daba337603948be53c67d207fbc2ad509aeabd6
 import { AuthProvider, useAuth } from './context/AuthContext';
 import { Web3Provider } from './context/Web3Context';
 import './i18n';
@@ -40,11 +36,7 @@ function App() {
                 <Route path="/flagged" element={<FlaggedTransactions />} />
                 <Route path="/admin" element={<ProtectedRoute role="admin"><AdminDashboard /></ProtectedRoute>} />
                 <Route path="/auditor" element={<ProtectedRoute role="auditor"><AuditorDashboard /></ProtectedRoute>} />
-<<<<<<< HEAD
-                <Route path="/prototypes" element={<FraudEdgeCases />} />
-=======
                 <Route path="/judges" element={<JudgesDashboard />} />
->>>>>>> 1daba337603948be53c67d207fbc2ad509aeabd6
               </Routes>
             </main>
           </div>
